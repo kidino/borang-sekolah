c9bcf9463f158861a419a94749c22ea9920340c4
Iszuddin Ismail <dino@websegera.my>
2020-01-09 14:25:35 +0800

    smoother keypress and backspace
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- js/app.js
+++ js/app.js
@@ -45,69 +45,63 @@ $(document).ready(function () {
 
     load_file_select();
 
-    $('body').on('keyup', 'input.s1', function (e) {
+    $('#reset_button').on('click', function () {
+        $('input').val('');
+
+    });    
+
+    $('body').on('input keypress', 'input.s1', function (e) {
 
         var $this = $(this);
         var $next_input;
+        console.log(e);
+        if (e.which != 8) {
+            if ($this.parent().next().find('input').length) {
+                $next_input = $this.parent().next().find('input');
+            } else if ($this.parent().next().next().find('input').length) {
+                $next_input = $this.parent().next().next().find('input');
+            } else if ($this.parent().parent().next().find('input:first').length) {
+                $next_input = $this.parent().parent().next().find('input:first');
+            } else if ($this.parent().parent().next().next().find('input:first').length) {
+                $next_input = $this.parent().parent().next().next().find('input:first');
+            } else if ($this.parent().parent().next().next().next().find('input:first').length) {
+                $next_input = $this.parent().parent().next().next().next().find('input:first');
+            } else if ($this.parent().parent().parent().next().find('input:first').length) {
+                $next_input = $this.parent().parent().parent().next().find('input:first');
+            } else if ($this.parent().parent().parent().parent().next().find('input:first').length) {
+                $next_input = $this.parent().parent().parent().parent().next().find('input:first');
+            }
 
-        $('#reset_button').on('click', function () {
-            $('input').val('');
+            if(e.currentTarget.value.length >= 1) {
+                $next_input.focus().select();
+            }
+        } 
 
-        });
+    }).on('keydown', 'input.s1', function(e){
+
+        var $this = $(this);
+        var $next_input;
 
         if (e.keyCode == 8) {
             $this.val('');
 
             if ($this.parent().prev().find('input').length) {
                 $next_input = $this.parent().prev().find('input');
-                console.log('prev a');
             } else if ($this.parent().prev().prev().find('input').length) {
                 $next_input = $this.parent().prev().prev().find('input');
-                console.log('prev b');
             } else if ($this.parent().parent().prev().find('input:last').length) {
                 $next_input = $this.parent().parent().prev().find('input:last');
-                console.log('prev c');
             } else if ($this.parent().parent().prev().prev().find('input:last').length) {
                 $next_input = $this.parent().parent().prev().prev().find('input:last');
-                console.log('prev d');
             } else if ($this.parent().parent().prev().prev().prev().find('input:last').length) {
                 $next_input = $this.parent().parent().prev().prev().prev().find('input:last');
-                console.log('prev e');
             } else if ($this.parent().parent().parent().prev().find('input:last').length) {
                 $next_input = $this.parent().parent().parent().prev().find('input:last');
-                console.log('prev f');
             } else if ($this.parent().parent().parent().parent().prev().find('input:last').length) {
                 $next_input = $this.parent().parent().parent().parent().prev().find('input:last');
-                console.log('prev g');
             }
-
-        } else {
-            if ($this.parent().next().find('input').length) {
-                $next_input = $this.parent().next().find('input');
-                console.log('a');
-            } else if ($this.parent().next().next().find('input').length) {
-                $next_input = $this.parent().next().next().find('input');
-                console.log('b');
-            } else if ($this.parent().parent().next().find('input:first').length) {
-                $next_input = $this.parent().parent().next().find('input:first');
-                console.log('c');
-            } else if ($this.parent().parent().next().next().find('input:first').length) {
-                $next_input = $this.parent().parent().next().next().find('input:first');
-                console.log('d');
-            } else if ($this.parent().parent().next().next().next().find('input:first').length) {
-                $next_input = $this.parent().parent().next().next().next().find('input:first');
-                console.log('e');
-            } else if ($this.parent().parent().parent().next().find('input:first').length) {
-                $next_input = $this.parent().parent().parent().next().find('input:first');
-                console.log('f');
-            } else if ($this.parent().parent().parent().parent().next().find('input:first').length) {
-                $next_input = $this.parent().parent().parent().parent().next().find('input:first');
-                console.log('g');
-            }
-
-        }
-        $next_input.focus().select();
-
+            $next_input.focus();//.select();
+        } 
     });
 
 });
